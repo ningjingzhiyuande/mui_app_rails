d6a332453308dbac0029861582be07818bb0f887
shiguodong <oreatial@gmail.com>
2015-05-23 18:44:50 +0800

    ff

--- lib/mui_app_rails/engine.rb
+++ lib/mui_app_rails/engine.rb
@@ -1,13 +1,13 @@
 module MuiAppRails
   class Engine < ::Rails::Engine
     isolate_namespace MuiAppRails
-    initializer 'font.assets.precompile' do |app|
+    initializer 'mui_app_rails.setup' do |app|
        
         %w(stylesheets javascripts fonts images).each do |sub|
           app.config.assets.paths << root.join('app','assets', sub).to_s
         end
 
-         app.config.assets.precompile << %r(fonts/\.(?:eot|svg|ttf|woff2?)$)
+        # app.config.assets.precompile << %r(fonts/\.(?:eot|svg|ttf|woff2?)$)
         
 
        # app.config.assets.precompile << /\.(?:svg|eot|woff|woff2|ttf)\z/
