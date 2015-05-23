fb480040dd6ec4e60c1b74da0af279e40992de72
shiguodong <oreatial@gmail.com>
2015-05-23 18:13:03 +0800

    ff

--- lib/mui_app_rails/engine.rb
+++ lib/mui_app_rails/engine.rb
@@ -6,6 +6,8 @@ module MuiAppRails
         %w(stylesheets javascripts fonts images).each do |sub|
           app.config.assets.paths << root.join('app','assets', sub).to_s
         end
+
+         app.config.assets.precompile << %r(fonts/\.(?:eot|svg|ttf|woff2?)$)
         
 
         app.config.assets.precompile << /\.(?:svg|eot|woff|woff2|ttf)\z/
