81cf835bfc0985c9374918fbe33c638a326b48f1
shiguodong <oreatial@gmail.com>
2015-05-23 17:30:38 +0800

    update

--- lib/mui_app_rails/engine.rb
+++ lib/mui_app_rails/engine.rb
@@ -1,5 +1,10 @@
 module MuiAppRails
   class Engine < ::Rails::Engine
     isolate_namespace MuiAppRails
+    initializer 'font.assets.precompile' do |app|
+        app.config.assets.paths << root.join('assets', "fonts").to_s
+        app.config.assets.precompile << /\.(?:svg|eot|woff|woff2|ttf)\z/
+    end
+
   end
 end
